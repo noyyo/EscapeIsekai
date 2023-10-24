f4f6185df8c5775834f45d761e34d47111bd394a
noyyo <dbswltjr7@gmail.com>
2023-10-24 20:46:34 +0900

    Init
    기본 프로젝트 생성입니다.

---
+++ Assets/Scripts/Managers/Singleton.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class CustomSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    // Check to see if we're about to be destroyed
+    private static bool _isShutdown = false;
+    private static T _instance;
+
+
+    public static T Instance
+    {
+        get
+        {
+            if (_isShutdown)
+            {
+                return null;
+            }
+
+            if (_instance == null)
+            {
+                _instance = (T)FindObjectOfType(typeof(T));
+
+                if (_instance == null)
+                {
+                    GameObject singletonObject = new GameObject { name = typeof(T).ToString() };
+                    _instance = singletonObject.AddComponent<T>();
+                    DontDestroyOnLoad(singletonObject);
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        _isShutdown = true;
+    }
+
+
+    private void OnDestroy()
+    {
+        _isShutdown = true;
+    }
+}
