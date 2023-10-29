cb67cc5017857fa67ddff2529361b4d14c671340
Shyunju <hyunju9908@naver.com>
2023-10-29 21:28:19 +0900

    [UPDATE] 월드 시간 게임메니저에서 사용가능 & 밤 밝기 조절

--- Assets/Scripts/Environments/Sky/DayNightCycle.cs
+++ Assets/Scripts/Environments/Sky/DayNightCycle.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
 {
-    [Range(0.0f, 1.0f)]
-    public float time; //하루 사이클 시간??
-    public float fulldayLength; //게임상 하루의 현실 시간(초)
-    public float startTime = 0.4f;
+    
+    [SerializeField] private float _fulldayLength; //게임상 하루의 현실 시간(초)
+    [SerializeField] private float _startTime = 0.4f;
     private float timeRate;
-    public Vector3 noon;
+
+    [SerializeField] private Vector3 _noon;
 
     [Header("Sun")]
     public Light sun;
@@ -27,27 +27,27 @@ public class DayNightCycle : MonoBehaviour
 
     private void Start()
     {
-        timeRate = 1.0f / fulldayLength; //얼마큼씩 변해야 하는지
-        time = startTime;
+        timeRate = 1.0f / _fulldayLength; //얼마큼씩 변해야 하는지
+        GameManager.Instance.time = _startTime;
     }
 
     private void Update()
     {
-        time = (time + timeRate * Time.deltaTime) % 1.0f;  //하루의 퍼센테지?
+        GameManager.Instance.time = (GameManager.Instance.time + timeRate * Time.deltaTime) % 1.0f;  //하루의 퍼센테지?
 
         UpdateLighting(sun, sunColor, sunIntensity);
         UpdateLighting(moon, moonColor, moonIntensity);
 
-        RenderSettings.ambientIntensity = lightingIntensityMultiplier.Evaluate(time); //환경광
-        RenderSettings.reflectionIntensity = reflectionintensityMultiplier.Evaluate(time); //반사광
+        RenderSettings.ambientIntensity = lightingIntensityMultiplier.Evaluate(GameManager.Instance.time); //환경광
+        RenderSettings.reflectionIntensity = reflectionintensityMultiplier.Evaluate(GameManager.Instance.time); //반사광
     }
 
     void UpdateLighting(Light lightSource, Gradient colorGradiant, AnimationCurve intensityCurve)
     {
-        float intensity = intensityCurve.Evaluate(time);
+        float intensity = intensityCurve.Evaluate(GameManager.Instance.time);
 
-        lightSource.transform.eulerAngles = (time - (lightSource == sun ? 0.25f : -0.75f)) * noon * 4.0f;
-        lightSource.color = colorGradiant.Evaluate(time);
+        lightSource.transform.eulerAngles = (GameManager.Instance.time - (lightSource == sun ? 0.25f : -0.75f)) * _noon * 4.0f;
+        lightSource.color = colorGradiant.Evaluate(GameManager.Instance.time);
         lightSource.intensity = intensity;
 
         GameObject go = lightSource.gameObject;
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public class GameManager : CustomSingleton<GameManager>
 {
-    
+    [Range(0.0f, 1.0f)]
+    public float time; //하루 사이클 시간  0.2~0.8 해떠있는 시간
 }
