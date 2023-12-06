70fc56e3d4fc04984764f6a3ab0f7ca49c578d0c
KimDoHyeon <hj2471755@naver.com>
2023-12-06 14:53:27 +0900

    [ADD] 붐버버그 프리팹 추가, 파티클효과 추가

---
+++ Assets/Scripts/Character/Enemy/Bomberbug.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Bomberbug : Enemy
+{
+    [Range(0f, 30f)][SerializeField] private float explosionRadius;
+    [Tooltip("폭발시 검출할 레이어")]
+    [SerializeField] private LayerMask layersToCollide;
+    [Tooltip("폭발범위 내의 검색 가능한 최대 객체의 수")]
+    [SerializeField] private int MaxCollisionObject = 20;
+    [SerializeField] private int damage = 20;
+    [SerializeField] private AttackEffectTypes attackEffectTypes;
+    [SerializeField] private float attackEffectValue = 1f;
+
+    private ExplosionManager explosionManager;
+
+    private new void Awake()
+    {
+        base.Awake();
+        explosionManager = ExplosionManager.Instance;
+        AnimEventReceiver.AnimEventCalled += AnimationEventDecision;
+    }
+
+    private void AnimationEventDecision(AnimationEvent animationEvent)
+    {
+        if(animationEvent.stringParameter == "Explosion")
+        {
+            Explosion();
+        }
+    }
+
+    public void Explosion()
+    {
+        explosionManager.Explosion(damage, attackEffectTypes, attackEffectValue, this.gameObject, this.transform.position, explosionRadius, layersToCollide, MaxCollisionObject);
+    }
+}
---
+++ Assets/Scripts/Explosion/ExplosionController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class ExplosionController : MonoBehaviour
+{
+    private IObjectPool<ExplosionController> managedPool;
+    private int damage;
+    private AttackEffectTypes attackEffectTypes;
+    private float attackEffectValue;
+    private GameObject attacker;
+    private LayerMask layer;
+    private Collider[] colliders;
+    private float explosiondelayTime;
+    private float explosionRadius;
+    private readonly string takeDamageString = "TakeDamage";
+    private ParticleSystem particleSystems;
+
+    private void Awake()
+    {
+        particleSystems = GetComponent<ParticleSystem>();
+    }
+
+    public void SetManagedPool(IObjectPool<ExplosionController> pool)
+    {
+        managedPool = pool;
+    }
+
+    public void Explosion()
+    {
+        Invoke(takeDamageString, explosiondelayTime);
+    }
+
+    private void TakeDamage()
+    {
+        particleSystems.Play(true);
+        int count = Physics.OverlapSphereNonAlloc(transform.position, explosionRadius, colliders, layer);
+        for (int i = 0; i < count; i++)
+        {
+            IDamageable target = null;
+            if (colliders[i].CompareTag(TagsAndLayers.PlayerTag))
+                target = colliders[i].GetComponent<Player>().StateMachine;
+            else
+                target = colliders[i].GetComponent<Enemy>().StateMachine;
+            target?.TakeDamage(damage);
+            target?.TakeEffect(attackEffectTypes, attackEffectValue, this.attacker);
+        }
+    }
+
+    public void ExplosionInit(Transform transform, Vector3 pos, float newExplosionRadius, LayerMask newlayer, int MaxCollisionObject = 20, float newExplosiondelayTime = 0)
+    {
+        this.transform.parent = transform;
+        this.transform.position = pos;
+        explosionRadius = newExplosionRadius;
+        layer = newlayer;
+        if(colliders == null)
+        {
+            colliders = new Collider[MaxCollisionObject];
+        }
+        else if(colliders.Length < MaxCollisionObject)
+        {
+            colliders = new Collider[MaxCollisionObject];
+        }
+        explosiondelayTime = newExplosiondelayTime;
+    }
+
+    public void ExplosionInitEnemyData(int newDamage, AttackEffectTypes newAttackEffectTypes, float newAttackEffectValue, GameObject newAttacker)
+    {
+        damage = newDamage;
+        attackEffectTypes = newAttackEffectTypes;
+        attackEffectValue = newAttackEffectValue;
+        attacker = newAttacker;
+    }
+
+    private void OnDisable()
+    {
+        managedPool.Release(this);
+    }
+}
