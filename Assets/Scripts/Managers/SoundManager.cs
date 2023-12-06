fa62a5cc91713f6291afc173da889d2a5bf381c3
KimDoHyeon <hj2471755@naver.com>
2023-12-06 15:52:27 +0900

    [FIX] 사운드매니저 초기화 문제 해결 및 AudioSource 반환 받을 수 있도록 수정

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.Pool;
@@ -36,17 +37,25 @@ public class SoundManager : CustomSingleton<SoundManager>
     private void Awake()
     {
         bgm = this.GetComponent<AudioSource>();
+        if(bgm == null )
+        {
+            bgm = this.AddComponent<AudioSource>();
+            mixer = Resources.Load<AudioMixer>("Sound/AudioVolumeController");
+            bgm.outputAudioMixerGroup = mixer.FindMatchingGroups("BGM")[0];
+        }
         CreateSoundList();
         sfxPrefab = Resources.Load<GameObject>("Prefabs/Sound/SFX");
         objectPool_AudioSources = new ObjectPool<SFX>(CreateSFX, OnGetSFX, OnReleasSFX, OnDestroySFX, maxSize: poolMaxCount);
         playLoopSFXList = new List<SFX>();
     }
+
     private void Start()
     {
         BGMPlay(ClipDics[0][defaultBGMName]);
         OnSoundAllStopEvent += BGMStop;
         OnSoundAllStopEvent += SFXAllStop;
     }
+
     private void CreateSoundList()
     {
         int i = 0;
@@ -165,22 +174,85 @@ public class SoundManager : CustomSingleton<SoundManager>
         }
         return false;
     }
+    //------------------------------------------------
+    public AudioSource CallPlaySFXReturnSource(ClipType clipType, string sfxName, Transform transform, bool isLoop)
+    {
+        if (ClipDics[(int)clipType].TryGetValue(sfxName, out AudioClip value))
+        {
+            return PlaySFXReturnSource(value, transform, value.length, isLoop, transform.position);
+        }
+        else
+        {
+            Debug.Log("Error 이 효과음과 같은 이름이 없습니다. 다시 확인해 주세요");
+            return null;
+        }
+    }
+
+    public AudioSource CallPlaySFXReturnSource(ClipType clipType, string sfxName, Vector3 vector3, bool isLoop)
+    {
+        if (ClipDics[(int)clipType].TryGetValue(sfxName, out AudioClip value))
+        {
+            return PlaySFXReturnSource(value, transform, value.length, isLoop, vector3);
+        }
+        else
+        {
+            Debug.Log("Error 이 효과음과 같은 이름이 없습니다. 다시 확인해 주세요");
+            return null;
+        }
+    }
+
+    public AudioSource CallPlaySFXReturnSource(ClipType clipType, string sfxName, Transform transform, float playTime)
+    {
+        if (ClipDics[(int)clipType].TryGetValue(sfxName, out AudioClip value))
+        {
+            return PlaySFXReturnSource(value, transform, playTime, false, transform.position);
+        }
+        else
+        {
+            Debug.Log("Error 이 효과음과 같은 이름이 없습니다. 다시 확인해 주세요");
+            return null;
+        }
+    }
+
+    public AudioSource CallPlaySFXReturnSource(ClipType clipType, string sfxName, Vector3 vector3, float playTime)
+    {
+        if (ClipDics[(int)clipType].TryGetValue(sfxName, out AudioClip value))
+        {
+            return PlaySFXReturnSource(value, transform, playTime, false, vector3);
+        }
+        else
+        {
+            Debug.Log("Error 이 효과음과 같은 이름이 없습니다. 다시 확인해 주세요");
+            return null;
+        }
+    }
+
+    private AudioSource PlaySFXReturnSource(AudioClip clip, Transform transform, float playTime, bool isLoop, Vector3 vector3)
+    {
+        SFX sfx = objectPool_AudioSources.Get();
+        sfx.PlaySFX(clip, transform, playTime, isLoop, vector3);
+        if (isLoop)
+            playLoopSFXList.Add(sfx);
+        OnSFXAllStopEvent += sfx.DestroyAudioSource;
+        return sfx.audioSource;
+    }
+
     //-------
 
     //사운드 조절
     public void MasterVolume(float val)
     {
-        mixer.SetFloat("MasterVolume", val - 80);
+        mixer.SetFloat("MasterVolume", val * 0.4f - 30);
     }
 
     public void BGMVolume(float val)
     {
-        mixer.SetFloat("BGMVolume", val - 80);
+        mixer.SetFloat("BGMVolume", val * 0.4f - 40);
     }
 
     public void SFXVolume(float val)
     {
-        mixer.SetFloat("SFXVolume", val - 80);
+        mixer.SetFloat("SFXVolume", val * 0.4f - 40);
     }
     //------
 
