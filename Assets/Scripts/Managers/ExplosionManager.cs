0212191425854e6fe11c08b13aa5222ffbab8c7f
subscriptioncat <119589973+subscriptioncat@users.noreply.github.com>
2023-12-06 14:55:26 +0900

    [ADD] 붐버버그 프리팹 추가, 파티클효과 추가 (#112)

---
+++ Assets/Scripts/Managers/ExplosionManager.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+using UnityEngine.Pool;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class ExplosionManager : CustomSingleton<ExplosionManager>
+{
+    protected ExplosionManager() { }
+    [SerializeField] private int poolMaxCount = 5;
+
+    private IObjectPool<ExplosionController> objectPool_ExplosionController;
+    private GameObject prefab;
+
+    private void Awake()
+    {
+        prefab = Resources.Load<GameObject>("Prefabs/Effect/Explosion");
+        objectPool_ExplosionController = new ObjectPool<ExplosionController>(CreateExplosionController, OnGetExplosion, OnReleasExplosion, OnDestroyExplosion, maxSize: poolMaxCount);
+    }
+
+    public void Explosion(int damage, AttackEffectTypes attackEffectTypes, float attackEffectValue, GameObject attacker, Vector3 pos, float explosionRadius, LayerMask layer, int MaxCollisionObject = 20, float explosiondelayTime = 0)
+    {
+        ExplosionController controller = objectPool_ExplosionController.Get();
+        controller.ExplosionInit(this.transform, pos, explosionRadius, layer, MaxCollisionObject, explosiondelayTime);
+        controller.ExplosionInitEnemyData(damage, attackEffectTypes, attackEffectValue, attacker);
+        controller.Explosion();
+    }
+
+    private ExplosionController CreateExplosionController()
+    {
+        ExplosionController explosion = Instantiate(prefab).GetComponent<ExplosionController>();
+        explosion.SetManagedPool(objectPool_ExplosionController);
+        return explosion;
+    }
+
+    private void OnGetExplosion(ExplosionController explosionController)
+    {
+        explosionController.gameObject.SetActive(true);
+    }
+    private void OnReleasExplosion(ExplosionController explosionController)
+    {
+        explosionController.gameObject.SetActive(false);
+    }
+
+    private void OnDestroyExplosion(ExplosionController explosionController)
+    {
+        Destroy(explosionController.gameObject);
+    }
+}
